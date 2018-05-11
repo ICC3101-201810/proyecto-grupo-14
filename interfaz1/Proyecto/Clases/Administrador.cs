40bcdbea0ecb53730cd270921d0713040dc11853
Alberto Altermatt <ajaltermatt@miuandes.cl>
2018-05-11 18:33:55 -0400

    e

--- interfaz1/Proyecto/Clases/Administrador.cs
+++ interfaz1/Proyecto/Clases/Administrador.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Proyecto
 {
+    [Serializable()]
     public class Administrador : Usuario
     {
         public string nombre;
--- interfaz1/Proyecto/Clases/Banco.cs
+++ interfaz1/Proyecto/Clases/Banco.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Proyecto
 {
+    [Serializable()]
     class Banco : Cliente
     {
         public string NroCuenta { get; set; }
